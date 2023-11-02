5e368731fb500494ff04e373900fa37148f7f2b1
Terjokan <Janosh@HTPC.fritz.box>
2023-11-02 21:22:34 +0100

    Durchfall damage

--- src/main/java/net/mcreator/durchfall/potion/DurchfallEffectMobEffect.java
+++ src/main/java/net/mcreator/durchfall/potion/DurchfallEffectMobEffect.java
@@ -1,8 +1,11 @@
 
 package net.mcreator.durchfall.potion;
 
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffect;
+
+import net.mcreator.durchfall.procedures.DurchfallEffectAktiveTickBedingungProcedure;
 
 public class DurchfallEffectMobEffect extends MobEffect {
 	public DurchfallEffectMobEffect() {
@@ -12,6 +15,13 @@ public class DurchfallEffectMobEffect extends MobEffect {
 	@Override
 	public String getDescriptionId() {
 		return "effect.durchfall.durchfall_effect";
+	}
+
+	@Override
+	public void applyEffectTick(LivingEntity entity, int amplifier) {
+		DurchfallEffectAktiveTickBedingungProcedure.execute(
+
+		);
 	}
 
 	@Override
